9a8330783a5cd9e68a4e1af8ff8d34ddc39a9e56
Darshan Crout <dtcrout@gmail.com>
2019-10-27 10:19:25 -0400

    Load CSV from Hello, world function and print songs to terminal

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ default:
 	go run main.go
 
 clean:
-	rm -rf *.html
+	rm -rf *.html *.html.*
--- main.go
+++ main.go
@@ -1,18 +1,64 @@
 package main
 
 import (
-	"io"
+	"encoding/csv"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	// Hello world handler function
-	hello_world := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+type Track struct {
+	Artist string
+	Song   string
+}
+
+func (t Track) String() string {
+	return fmt.Sprintf("Artist: %s, Song: %s", t.Artist, t.Song)
+}
+
+func ReadCSV(filename string) [][]string {
+	// Open CSV file
+	f, _ := os.Open(filename)
+
+	// Close file
+	defer f.Close()
+
+	// Create reader
+	reader := csv.NewReader(f)
+
+	// Read all records
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", hello_world)
+	return records
+}
 
+// Hello world handler function
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	lines := ReadCSV("resources/tracks_2.csv")
+
+	// Create slice to hold tracks
+	var tracks []Track
+
+	for i, line := range lines {
+		// Skip header
+		if i == 0 {
+			continue
+		}
+		track := Track{Artist: line[0], Song: line[1]}
+		tracks = append(tracks, track)
+	}
+
+	for _, track := range tracks {
+		fmt.Println(track)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", HelloWorld)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
